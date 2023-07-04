3404a75dfc6f1faadeb654734e66391c6f1c88f8
recklesssss <110936205+recklesssss@users.noreply.github.com>
2023-07-05 02:00:48 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      animation: {
+        "spin-slow": "spin 2.8s linear infinite",
+      },
+      colors: {
+        primary: "#bc4fff",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
